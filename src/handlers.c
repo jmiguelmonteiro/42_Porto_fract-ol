7716731eb705a776216be8f4490dca8a3cefd8f6
josemigu <josemigu@student.42porto.com>
2025-06-18 14:46:12 +0100

    20250618

--- src/handlers.c
+++ src/handlers.c
@@ -6,11 +6,34 @@
 /*   By: josemigu <josemigu@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/10 15:14:48 by josemigu          #+#    #+#             */
-/*   Updated: 2025/06/13 16:55:56 by josemigu         ###   ########.fr       */
+/*   Updated: 2025/06/18 14:44:37 by josemigu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractol.h"
+
+static void	handle_color_shift(int keysym, t_fractol *f)
+{
+	(void)f;
+	if (keysym == 91)
+		;
+	else if (keysym == 93)
+		;
+}
+
+static void	handle_iterations(int keysym, t_fractol *f)
+{
+	if (keysym == 61)
+	{
+		if (f->max_iterations * 1.2 <= 1200)
+			f->max_iterations *= 1.2;
+	}
+	else if (keysym == 45)
+	{
+		if (f->max_iterations * 0.8 >= 20)
+			f->max_iterations *= 0.8;
+	}
+}
 
 int	handle_close(t_fractol *f)
 {
@@ -20,7 +43,6 @@ int	handle_close(t_fractol *f)
 
 int	handle_key_input(int keysym, t_fractol *f)
 {
-	ft_printf("The %d key has been pressed!!!\n", keysym);
 	if (keysym == XK_Escape)
 		handle_close(f);
 	else if (keysym == 65363)
@@ -31,16 +53,10 @@ int	handle_key_input(int keysym, t_fractol *f)
 		f->offset_y -= 0.5 * f->zoom;
 	else if (keysym == 65364)
 		f->offset_y += 0.5 * f->zoom;
-	else if (keysym == 43)
-	{
-		if (f->max_iterations * 1.2 <= 1200)
-			f->max_iterations *= 1.2;
-	}
-	else if (keysym == 45)
-	{
-		if (f->max_iterations * 0.8 >= 20)
-			f->max_iterations *= 0.8;
-	}
+	else if (keysym == 45 || keysym == 61)
+		handle_iterations(keysym, f);
+	else if (keysym == 91 || keysym == 93)
+		handle_color_shift(keysym, f);
 	else if (keysym == XK_r)
 		init_data(f);
 	draw_fractol(f);
